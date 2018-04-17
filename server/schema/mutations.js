9c911c30818d5f86ffa45537506d2aeb9668e0ed
Iswan Jumat <iswan@thinkcube.com>
2018-04-17 22:11:35 +0530

    added mark attendance mutation

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -9,7 +9,9 @@ const {
   GraphQLNonNull
 } = graphql;
 const Employee = mongoose.model("employee");
+const Attendance = mongoose.model("attendance");
 const EmployeeType = require("./employee_type");
+const AttendanceType = require("./attendance_type");
 
 const mutation = new GraphQLObjectType({
   name: "Mutation",
@@ -26,10 +28,25 @@ const mutation = new GraphQLObjectType({
         createdAt: { type: GraphQLString },
         casualLeaves: { type: GraphQLInt },
         annualLeaves: { type: GraphQLInt },
-        sickLeaves: { type: GraphQLInt }
+        sickLeaves: { type: GraphQLInt },
+        email: { type: new GraphQLNonNull(GraphQLString) },
+        password: { type: GraphQLString }
       },
       resolve(parentValue, args) {
         return new Employee(args).save();
+      }
+    },
+    markAttendance: {
+      type: AttendanceType,
+      args: {
+        type: { type: new GraphQLNonNull(GraphQLString) },
+        date: { type: new GraphQLNonNull(GraphQLString) },
+        time: { type: new GraphQLNonNull(GraphQLString) },
+        status: { type: GraphQLInt },
+        employee: { type: new GraphQLNonNull(GraphQLID) }
+      },
+      resolve(parentValue, args) {
+        return new Attendance(args).save();
       }
     }
   }
